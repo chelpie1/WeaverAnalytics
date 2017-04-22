cleaned up pgm scaling, running algorithm now, still needs FIXING THOUG HCHELSEA!!!!!!!!!!!

LPCA_SRC_Classify.py
@@ -1,26 +1,28 @@
 import numpy as np
 import pandas as pd
 from algorithms import l1l2_regularization
+import os
 
+os.chdir('data')
 # Load in trained classification model:
-DICT_full = np.array(pd.read_csv('DICT_full.dat', header=None))
-lab_DICT_full = np.array(pd.read_csv('lab_DICT_full.dat', header=None))
-PCA_mapping = np.array(pd.read_csv('PCA_mapping.dat', header=None))
-r_1 = float(np.array(pd.read_csv('r_1.dat', header=None)))
-train_ind = np.array(pd.read_csv('train_ind.dat', header=None))
-train_mean = np.array(pd.read_csv('train_mean.dat', header=None))
-train_pt_ind = np.array(pd.read_csv('train_pt_ind.dat', header=None))
+DICT_full = np.array(pd.read_csv(r'DICT_full.dat', header=None))
+lab_DICT_full = np.array(pd.read_csv(r'lab_DICT_full.dat', header=None))
+PCA_mapping = np.array(pd.read_csv(r'PCA_mapping.dat', header=None))
+r_1 = float(np.array(pd.read_csv(r'r_1.dat', header=None)))
+train_ind = np.array(pd.read_csv(r'train_ind.dat', header=None))
+train_mean = np.array(pd.read_csv(r'train_mean.dat', header=None))
+train_pt_ind = np.array(pd.read_csv(r'train_pt_ind.dat', header=None))
 
 # Reformat:
-train_mean = np.reshape(train_mean, (32256,))
+train_mean = np.reshape(train_mean, (32256,1))
 train_pt_ind = [i-1 for [i] in train_pt_ind]
 
 # Load in test data:
 A_test_orig = np.array(pd.read_csv('A_test_orig.dat', header=None))
-
+os.chdir('..')
 
 def LPCA_SRC_Classify(y):
-
+    print('prep')
     # Prep variables:
     DICT_y = np.zeros((np.shape(DICT_full)))
     row_num, col_num = np.shape(DICT_y)
@@ -30,7 +32,8 @@ def LPCA_SRC_Classify(y):
     n_train = len(train_pt_ind)
     L = len(np.unique(lab_DICT_full))  # Number of classes
     d_vect = np.ones((L, 1))
-
+	
+    print('reshape')
     # Reshape and extract PCA features from test sample:
     if np.shape(y) == (32256,):
         pass
@@ -39,9 +42,15 @@ def LPCA_SRC_Classify(y):
         y = np.reshape(y, (dim1*dim2, 1))
     else:
         ValueError('Image does not have the correct number of pixels.')
+    
+    print('reshape2')
+    print( y.shape )
+    print( train_mean.shape )
+    print( (y-train_mean).shape)
     y_fea = np.matmul(PCA_mapping.T, y - train_mean)
     y_norm = y_fea/np.linalg.norm(y_fea)
-
+	
+    print('distances')
     # Compute distances between the test point and each training point:
     dist_vects_pos = np.tile(y_norm, (n_train, 1)).T - X_train_norm
     DIST_pos = np.sqrt(sum(dist_vects_pos ** 2))
@@ -55,6 +64,7 @@ def LPCA_SRC_Classify(y):
     # Set dictionary pruning parameter:
     r = max(r_1, r_2)
 
+    print('dictionary')
     # Amend dictionary to include only training points (and their corresponding tangent basis vectors) that are within
     # r of the sample:
     ind_pos = np.asarray(np.where(DIST_pos <= r))
@@ -71,7 +81,8 @@ def LPCA_SRC_Classify(y):
     count = 0
     ind = list(ind)
     close_train_pts = [train_pt_ind[i] for i in ind]  # index of nearby training points in DICT_full
-
+	
+    print('loop')
     for i in range(n_full):  # For each nearby training point
         ctp_i = int(close_train_pts[i])
         class_l_index = int(lab_DICT_full[0, ctp_i])  # class label of this training sample
@@ -86,7 +97,7 @@ def LPCA_SRC_Classify(y):
     n_y = len(DICT_y.T)
 
     # l1-Minimization:#####################################################
-
+    print('minimization')
     x = l1l2_regularization(DICT_y, y_norm, 0, 1e-5)
 
     # Compute class error for each class:
@@ -109,11 +120,11 @@ def LPCA_SRC_Classify(y):
 #        5. Clean up code, make look nice
 #        6. Consider how we want to display output (maybe ask Ryan his opinion)
 
-for i in range(20):
-    cl_info = LPCA_SRC_Classify(A_test_orig[: ,i])
-    print("The predicted class of the test sample is {}.".format(cl_info[0]))
-    print("The residual for each class is as follows:")
-    for l in range(10):
-        print("The class {} residual is {:.2}".format(l+1, cl_info[1][l]))
-    print("\n")
+# for i in range(20):
+    # cl_info = LPCA_SRC_Classify(A_test_orig[: ,i])
+    # print("The predicted class of the test sample is {}.".format(cl_info[0]))
+    # print("The residual for each class is as follows:")
+    # for l in range(10):
+        # print("The class {} residual is {:.2}".format(l+1, cl_info[1][l]))
+    # print("\n")
 

app.py
@@ -18,15 +18,17 @@ app.debug = True
 imageDict = {}
 
 facialPath = r'C:\Users\Ryan\Desktop\WeaverAnalytics\static\FacialImages\\'
-for classes in os.listdir(facialPath)[0:4]:
-	
-	files = [x for x in os.listdir( facialPath + classes ) if x[-3:] == 'pgm' ][0:5] 
+for classes in os.listdir(facialPath)[0:5]:
+	print(classes)
+	files = [x for x in os.listdir( facialPath + classes ) if x[-3:] == 'pgm' ][0:10] 
 	
 	temp = []
 	for x in files:
+		#load up image
 		iMatrix =  np.array(Image.open(facialPath + classes + '\\' + x)).flatten().tolist()
-		place = np.array(Image.open(facialPath + classes + '\\' + x))
+		#convert to rgba matrix, probably not necessary?
 		iMatrix = [ v for v in iMatrix for _ in (0,1,2,3)  ] 
+		#set alpha
 		iMatrix[3::4] = int(len(iMatrix)/4)*[255]
 		w = [ x, json.dumps(iMatrix) ]
 		
@@ -52,11 +54,11 @@ def imageAnalysis():
 	img_dict = re.match("data:(?P<type>.*?);(?P<encoding>.*?),(?P<data>.*)", request.form['image']).groupdict()
 	blob = img_dict['data'] 
 
-	im = Image.open(BytesIO(base64.b64decode(blob))).convert('L').resize(  (192,168  ) , PIL.Image.ANTIALIAS) 
+	im = Image.open(BytesIO(base64.b64decode(blob))).convert('L').resize(  (168,192  ) , PIL.Image.ANTIALIAS) 
 	matrix = np.array(im)
-	
+	print( matrix.shape )
 	label = classifier.LPCA_SRC_Classify(matrix)
-	
+
 	return '0' 
 	
 