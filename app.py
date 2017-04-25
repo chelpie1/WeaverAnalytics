68deb8046e9d46d9b9359ec39f48f0a4243dee06
Ryan <ryanhalabi@gmail.com>
2017-04-25 00:55:56 -0600

    added output of who you match with

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ from io import BytesIO
 import LPCA_SRC_Classify as classifier
 
 app = Flask(__name__ )
-app.debug = False
+app.debug = True
 
 imageDict = {}
 
-facialPath = r'C:\Users\chelpie\Desktop\WeaverAnalytics\static\FacialImages\\'
-for classes in os.listdir(facialPath)[0:5]:
+facialPath = r'C:\Users\Ryan\Desktop\WeaverAnalytics\static\FacialImages\\'
+for classes in os.listdir(facialPath)[0:10]:
 	print(classes)
-	files = [x for x in os.listdir( facialPath + classes ) if x[-3:] == 'pgm' ][0:10] 
+	files = [x for x in os.listdir( facialPath + classes ) if x[-3:] == 'pgm' ][0:5] 
 	
 	temp = []
 	for x in files:
@@ -58,10 +58,14 @@ def imageAnalysis():
 	matrix = np.array(im)
 	print( matrix.shape )
 	label = classifier.LPCA_SRC_Classify(matrix)
-
-	return '0' 
+	
+	who = label[0]
+	weights = label[1]
+	print(who)
+	print(weights)
+	return str(who)
 	
 
 
 if __name__ == "__main__":
-    app.run(host = '192.168.111.1',port=5015)
+    app.run(host = '192.168.0.106',port=5015)
